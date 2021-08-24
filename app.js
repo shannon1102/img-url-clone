d0cca6fff5b5563d65be65958e563a8102f58939
shannon1102 <vanictk62@gmail.com>
2021-08-25 03:51:37 +0700

    delete js

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ var logger = require('morgan');
 require('dotenv').config()
 var indexRouter = require('./routes/index');
 var imagesRouter = require('./routes/images');
-
+const cors = require('cors');
 var app = express();
-var $ = require('jquery')
 
-app.set('/public', path.join(__dirname, 'public'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,6 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+
 
 app.use('/', indexRouter);
 app.use('/images', imagesRouter);
@@ -30,15 +30,15 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  console.log("???")
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   console.log("???")
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
